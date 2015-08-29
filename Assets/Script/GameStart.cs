17ad7f2cd4e9e22e2052231ee71593cceddf56dd
Sangliang <378305868@qq.com>
2015-08-29 20:45:32 +0800

    sa

---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+    //这个函数放在Camera上，在点击“开始游戏”按钮的时候通过onclick控件，
+    //找到camera对象，再调用下面的GameStartButton方法跳转场景
+    public void GameStartButton() {
+        Application.LoadLevel(1);
+    }
+}
